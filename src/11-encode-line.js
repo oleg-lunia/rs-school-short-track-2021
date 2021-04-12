8e03a9d62af33a4d2d1c7d2c3697d4bb53606f6a
Oleg Lunia <oleglunja2@gmail.com>
2021-04-12 17:08:22 +0300

    refactoring: completed task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let n = 1;
+  const stack = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      n++;
+    } else if (arr[i] !== arr[i + 1] && arr[i] === arr[i - 1]) {
+      stack.push(`${n}${arr[i]}`);
+      n = 1;
+    } else if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
+      stack.push(arr[i]);
+    }
+  }
+
+  const newStr = stack.join('');
+
+  return newStr;
 }
 
 module.exports = encodeLine;
